2bc7973d886fccd97793b91435b5f5529be007e8
javi <javier.bravoz@mail.udp.cl>
2024-02-26 13:20:54 -0300

    r

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -13,12 +13,12 @@ return (
             After that i made a bootcamp of react, express.js, postgresSQL <br />
             testing, dockers and ci/cd, i learned rust by reading documentation + some books <br />
             and finally i learned t3stack that is next, prisma and trpc, i learned with a 15 years developer who teached me. <br />
-             i'm used to work with SCRUM metodology. <br />
+             i am used to work with SCRUM metodology. <br />
             <br />
             things you should know about me: <br />
-            i'm a yoga slackliner <br />
+            i am a yoga slackliner <br />
             i love to play dota2 <br />
-            i'm a didgeridoo player <br />
+            i am a didgeridoo player <br />
             i used to be a climber.    
         </p>
     </div>
