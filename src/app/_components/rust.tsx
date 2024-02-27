63c56372cf39fc03ec07a1a613fec39faa22ce29
javi <javier.bravoz@mail.udp.cl>
2024-02-27 04:08:59 -0300

    c

--- src/app/_components/rust.tsx
+++ src/app/_components/rust.tsx
@@ -11,11 +11,11 @@ return (
         <p className="text-gray 600 rustp">
           On rust i read and practiced the entire  book of The Rust Programming Language <br />
           i also made some little softwares like a text scrapper to find some phrases or keywords, <br />
-          text formatters, an Api with drinks and their recipes , among others. I'm currently learning <br />
+          text formatters, an Api with drinks and their recipes , among others. I am currently learning <br />
           real uses for multithreads
           <br />
           i studied smart pointers, data structures, advanced types, traits, hashmaps and other rust features. <br />
-          it's not my best language but i'm getting into it.    
+          it is not my best language but i am getting into it.    
         </p>
     </div>
   </div>
