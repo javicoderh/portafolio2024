1756b70450d76f3e19094571cf576330cbb06876
javi <javier.bravoz@mail.udp.cl>
2024-02-27 04:44:38 -0300

    c

--- src/app/_components/next.tsx
+++ src/app/_components/next.tsx
@@ -11,8 +11,9 @@ return (
         <p className="text-gray 600 rustp">
           Next is actually my best stack, working together with prisma and trpc i guess is a very <br />
           functional stack specially because you can still use all the resources from React and Js. <br />
-          If you check the project on the latest project section, you can find in Vuelca most of the features from <br />
-          next.js, prisma and trpc, also you can find my html, css, react and postgres knowledge. <br /> 
+          If you check the project on the latest project section, you can find in Vuelca most of the <br /> 
+          features  from next.js, prisma and trpc, also you can find my html, css, react and postgres <br /> 
+          knowledge. 
           <br />
           <br />
           This project was made using Next.js 
