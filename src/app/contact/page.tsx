fa174a371e47dbf5da5431fd4fa54756600731b3
javi <javier.bravoz@mail.udp.cl>
2024-03-18 11:39:00 -0300

    s

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -16,11 +16,13 @@ return (
     <div className="flex contact">
         <h1 className="hidden md:block ml-5 mb-2 mt-3 horizontal-tit">Contact</h1>
         <h1 className="block md:hidden ml-5 mb-2 mt-3 mobile-tit">Contact:</h1>
+        <Link href="https://github.com/javicoderh" target="_blank">
         <div className="flex md:hidden item-container">
         <Image src={github} width={50} height={100} alt="" />
         <h3>/javicorderh</h3>
        </div>
-       <Link href="https://www.linkedin.com/in/javierzapico/" target="_blank">
+       </Link>
+       <Link href="/https://www.linkedin.com/in/javierzapico/" target="_blank">
        <div className="flex md:hidden item-container">        
         <Image src={linkedin} width={50} height={100} alt="" />
         <h3>/javierzapico</h3>        
@@ -38,18 +40,24 @@ return (
        </div>
         <h1 className="hidden md:block text-gray-700 ml-5 mb-2 mt-3 vertical-tit">C <br /> O <br /> N <br />  T <br /> A <br />  C <br /> T</h1>
       <div className="hidden: md:flex items-container">
+        <Link href="https://github.com/javicoderh" target="_blank">
        <div className="hidden md:flex item-container">
         <Image src={github} width={50} height={100} alt="" />
         <h3>/javicorderh</h3>
        </div>
+       </Link>
+       <Link href="https://www.linkedin.com/in/javierzapico/" target="_blank">
        <div className="hidden md:flex item-container">
         <Image src={linkedin} width={50} height={100} alt="" />
         <h3>/javierzapico</h3>
        </div>
+       </Link>
+       <a href="mailto:javier.bravoz@mail.udp.cl"> 
        <div className="hidden md:flex item-container">
         <Image src={gmail} width={50} height={100} alt="" />
         <h3>javier.bravoz@mail.udp.cl</h3>
        </div>
+       </a>
        <div className="hidden md:flex item-container">
         <Image src={cellphone} width={50} height={100} alt="" />
         <h3>+56920536330</h3>        
