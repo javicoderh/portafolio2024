85b6a7db8ff22e7d3a28e997505511240643f029
javi <javier.bravoz@mail.udp.cl>
2024-03-18 11:22:03 -0300

    s

--- src/app/_components/reactMobile.tsx
+++ src/app/_components/reactMobile.tsx
@@ -10,7 +10,7 @@ return (
           React was my favourite framework until i met next.js, i feel very comfortable working with it, <br />
           and i can use most of react features, such as contexts, router, states, effects, lazy react and some libraries<br />
           like redux and material ui, also i can use all the features from javascript and i can add typescript<br /> 
-          to my react apps, and test them with jest or react-test, i can develop pipelines for react app <br /> 
+          to my react apps, and test them with jest or react test, i can develop pipelines for react app <br /> 
           for CI/CD and conect my apps into virtual machines using docker.
       </p>
     </div>
